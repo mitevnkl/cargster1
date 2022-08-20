7f74326f33ee1c40e1a24d04d4d9e4b846eb0835
Nikola Mitev <mitev.nkl@gmail.com>
2022-08-20 16:52:40 +0200

    repost done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,8 @@
 class PostsController < ApplicationController
-
-
-  def new
-    @post = Post.new
-    @countries = ["AL 🇦🇱 Albania", "AD 🇦🇩 Andorra", "AM 🇦🇲 Armenia", "AT 🇦🇹 Austria", "AZ 🇦🇿 Azerbaijan", "BA 🇧🇦 Bosnia and Herzegovina", "BE 🇧🇪 Belgium", "BG 🇧🇬 Bulgaria", "BY 🇧🇾 Belarus", "CH 🇨🇭 Switzerland", "CY 🇨🇾 Cyprus", "CZ 🇨🇿 Czech Republic", "DE 🇩🇪 Germany", "DK 🇩🇰 Denmark", "EE 🇪🇪 Estonia", "ES 🇪🇸 Spain", "FI 🇫🇮 Finland", "FR 🇫🇷 France", "GE 🇬🇪 Georgia", "GR 🇬🇷 Greece", "HR 🇭🇷 Croatia", "HU 🇭🇺 Hungary", "IE 🇮🇪 Ireland", "IS 🇮🇸 Iceland", "IT 🇮🇹 Italy", "LV 🇱🇻 Latvia", "LI 🇱🇮 Liechtenstein", "LT 🇱🇹 Lithuania", "LU 🇱🇺 Luxembourg", "MK 🇲🇰 Macedonia", "MD 🇲🇩 Moldova", "ME 🇲🇪 Montenegro", "NL 🇳🇱 Netherlands", "NO 🇳🇴 Norway", "PL 🇵🇱 Poland", "PT 🇵🇹 Portugal", "RO 🇷🇴 Romania", "RS 🇷🇸 Serbia", "RU 🇷🇺 Russia", "SE 🇸🇪 Sweden", "SI 🇸🇮 Slovenia", "SK 🇸🇰 Slovakia", "TR 🇹🇷 Turkey", "UA 🇺🇦 Ukraine", "UK 🇬🇧 United Kingdom", "XK 🇽🇰 Kosovo"]
-  end
-
   def index
-  @posts = Post.where(nil)
-  filtering_params(params).each do |key, value|
-    @posts = @posts.public_send("filter_by_#{key}", value) if value.present?
-  end
-
-  def show
-    @post = Post.find(params[:id])
-    @user = Post.find(params[:id]).user
-    @posts = Post.where(user: @user)
-
+    @posts = Post.where(nil)
+    filtering_params(params).each do |key, value|
+      @posts = @posts.public_send("filter_by_#{key}", value) if value.present?
   end
 
   def create
@@ -28,8 +14,23 @@ class PostsController < ApplicationController
     else
       render :new
     end
-
   end
+
+  def new
+    if params[:repost].present?
+      @post = Post.new(load_c: params[:repost][:load_c], load_city: params[:repost][:load_city], load_address: params[:repost][:load_address], unload_c: params[:repost][:unload_c], unload_city: params[:repost][:unload_city], unload_address: params[:repost][:unload_address], length: params[:repost][:length], weight: params[:repost][:weight], description: params[:repost][:description], truck_type: params[:repost][:truck_type], price: params[:repost][:price])
+    else
+      @post = Post.new
+    end
+    @my_posts = Post.where(user_id: current_user.id)
+    @countries = ["AL 🇦🇱 Albania", "AD 🇦🇩 Andorra", "AM 🇦🇲 Armenia", "AT 🇦🇹 Austria", "AZ 🇦🇿 Azerbaijan", "BA 🇧🇦 Bosnia and Herzegovina", "BE 🇧🇪 Belgium", "BG 🇧🇬 Bulgaria", "BY 🇧🇾 Belarus", "CH 🇨🇭 Switzerland", "CY 🇨🇾 Cyprus", "CZ 🇨🇿 Czech Republic", "DE 🇩🇪 Germany", "DK 🇩🇰 Denmark", "EE 🇪🇪 Estonia", "ES 🇪🇸 Spain", "FI 🇫🇮 Finland", "FR 🇫🇷 France", "GE 🇬🇪 Georgia", "GR 🇬🇷 Greece", "HR 🇭🇷 Croatia", "HU 🇭🇺 Hungary", "IE 🇮🇪 Ireland", "IS 🇮🇸 Iceland", "IT 🇮🇹 Italy", "LV 🇱🇻 Latvia", "LI 🇱🇮 Liechtenstein", "LT 🇱🇹 Lithuania", "LU 🇱🇺 Luxembourg", "MK 🇲🇰 Macedonia", "MD 🇲🇩 Moldova", "ME 🇲🇪 Montenegro", "NL 🇳🇱 Netherlands", "NO 🇳🇴 Norway", "PL 🇵🇱 Poland", "PT 🇵🇹 Portugal", "RO 🇷🇴 Romania", "RS 🇷🇸 Serbia", "RU 🇷🇺 Russia", "SE 🇸🇪 Sweden", "SI 🇸🇮 Slovenia", "SK 🇸🇰 Slovakia", "TR 🇹🇷 Turkey", "UA 🇺🇦 Ukraine", "UK 🇬🇧 United Kingdom", "XK 🇽🇰 Kosovo"]
+  end
+
+  # def show
+  #   @post = Post.find(params[:id])
+  #   @user = Post.find(params[:id]).user
+  #   @posts = Post.where(user: @user)
+  # end
 
   end
 
