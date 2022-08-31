20331379c94816145388e7493571f58f96b11a29
Nikola Mitev <mitev.nkl@gmail.com>
2022-08-31 15:57:49 +0200

    sweet alert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,16 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
+
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+initSweetalert('#sweet-alert-demo', {
+  title: "Are you sure?",
+  text: "This action cannot be reversed",
+  icon: "warning"
+}, (value) => {
+  if (value) {
+    const link = document.querySelector('#delete-link');
+    link.click();
+  }
+});
