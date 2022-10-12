ae51a54f7bdb65425cb19db6f5ecb2edb9183007
Nikola Mitev <mitev.nkl@gmail.com>
2022-10-12 17:25:52 +0200

    cb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,10 +42,10 @@ Rails.application.configure do
   # password: ENV["EMAIL_PASSWORD"]
   # }
 
-  config.action_mailer.delivery_method = :sendmail
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'nikola@cargster.com'}
+  config.action_mailer.default_options = {from: 'noreply@cargster.com'}
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
@@ -54,7 +54,7 @@ Rails.application.configure do
     :port           => 587,
     :domain         => 'privateemail.com',
     :authentication => :plain,
-    :user_name      => 'nikola@cargster.com',
+    :username       => ENV["EMAIL"],
     :password       => ENV["EMAIL_PASSWORD"]
   }
 
