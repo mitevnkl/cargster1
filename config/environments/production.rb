41131d8a2f212654d353fc3c16c5befd835147ac
Nikola Mitev <mitev.nkl@gmail.com>
2022-10-12 17:08:18 +0200

    cb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # config.action_mailer.default_url_options = { host: "http://www.cargster.com" }
+  config.action_mailer.default_url_options = { host: "https://www.cargster.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
